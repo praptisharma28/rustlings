e9e4955ebdc91f5f70a892b0fbcb376cbf61a2e3
Prapti Sharma <praptisharma282003@gmail.com>
2025-07-23 20:40:03 +0530

    04_primitive types

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -10,7 +10,7 @@ mod tests {
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
-
+        let nice_slice = &a[1..4]; // in rust, slicing can only be taken as a reference and it means refering to where the original array refers without copying or moving
         assert_eq!([2, 3, 4], nice_slice);
     }
 }
--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -3,6 +3,6 @@ fn main() {
 
     // TODO: Destructure the `cat` tuple in one statement so that the println works.
     // let /* your pattern here */ = cat;
-
+    let (name, age) = cat; // destructuring the tuple into name and age variables
     println!("{name} is {age} years old");
 }
