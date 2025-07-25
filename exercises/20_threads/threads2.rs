1314e6600b0dbac12d96a51fdd4ab51088db4c54
Prapti Sharma <praptisharma282003@gmail.com>
2025-07-26 02:26:37 +0530

    .

--- exercises/20_threads/threads2.rs
+++ exercises/20_threads/threads2.rs
@@ -1,16 +1,12 @@
-// Building on the last exercise, we want all of the threads to complete their
-// work. But this time, the spawned threads need to be in charge of updating a
-// shared value: `JobStatus.jobs_done`
-
-use std::{sync::Arc, thread, time::Duration};
+use std::{sync::{Arc, Mutex}, thread, time::Duration};
 
 struct JobStatus {
     jobs_done: u32,
 }
 
 fn main() {
-    // TODO: `Arc` isn't enough if you want a **mutable** shared state.
-    let status = Arc::new(JobStatus { jobs_done: 0 });
+    // Wrap JobStatus in a Mutex to allow safe, shared mutability.
+    let status = Arc::new(Mutex::new(JobStatus { jobs_done: 0 }));
 
     let mut handles = Vec::new();
     for _ in 0..10 {
@@ -18,17 +14,17 @@ fn main() {
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
 
-            // TODO: You must take an action before you update a shared value.
-            status_shared.jobs_done += 1;
+            // Lock the mutex before modifying the shared state.
+            let mut status = status_shared.lock().unwrap();
+            status.jobs_done += 1;
         });
         handles.push(handle);
     }
 
-    // Waiting for all jobs to complete.
     for handle in handles {
         handle.join().unwrap();
     }
 
-    // TODO: Print the value of `JobStatus.jobs_done`.
-    println!("Jobs done: {}", todo!());
+    // Lock again to read safely.
+    println!("Jobs done: {}", status.lock().unwrap().jobs_done);
 }
